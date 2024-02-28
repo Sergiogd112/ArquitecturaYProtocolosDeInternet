5880f8e26271d05eaaa88c08a3230c221014f642
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2024-02-28 17:12:22 +0100

    Update installer.sh script to include -y flag for apt-get install commands

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,10 +3,10 @@
 echo "Installing the project"
 echo "Installing python3"
 sudo apt update
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
-sudo apt-get install build-essential checkinstall
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
+sudo apt-get install build-essential checkinstall -y
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
-    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
 cd /opt
 sudo wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
 sudo tar xzf Python-3.8.12.tgz
