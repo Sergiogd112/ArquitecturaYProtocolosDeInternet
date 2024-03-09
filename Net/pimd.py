1a1f39b316a4f65d85c99dca3edb01273138e6dc
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2024-03-09 11:08:47 +0100

    Refactor PIMD class initialization and add type hints

--- Net/pimd.py
+++ Net/pimd.py
@@ -1,5 +1,8 @@
 class PIMD:
-    def __init__(self,groups:dict={}):
-        self.groups = groups
-    def load_pimd_config(self,config:str):
+    def __init__(self, groups: dict = None):
+        if groups is None:
+            groups = {}
+        self.groups: dict = groups
+
+    def load_pimd_config(self, config: str):
         pass
