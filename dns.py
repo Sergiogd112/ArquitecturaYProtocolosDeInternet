2ce7bb49741f53f0795bea4bff876ba971fed0e0
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2024-04-22 18:59:02 +0200

    Add toml library and DNS configuration files

--- dns.py
+++ dns.py
@@ -1,20 +1,120 @@
+from typing import List
 from rich.prompt import Prompt, IntPrompt
+from rich.console import Console
+import tomllib
 
 
-class RR:
-    def __init__(self, origin, TTL, dns_name, admin, inv_origin):
+class RRTable:
+    def __init__(self, origin, ttl, dns_name, admin, inv_origin, ip):
         self.origin = origin
-        self.TTL = TTL
+        self.ttl = ttl
         self.dns_name = dns_name
         self.admin = admin
         self.inv_origin = inv_origin
-        self.NS = []
-        self.inv_NS = []
-        self.MX = []
-        self.inv_MX = []
+        self.octets = 7 - len(inv_origin.split("."))
+        self.ip = ip
+        self.inv_name = (
+            self.dns_name
+            if self.dns_name.endswith(".")
+            else self.dns_name + "." + self.origin
+        )
+        self.inv_admin = (
+            self.admin if self.admin.endswith(".") else self.admin + "." + self.origin
+        )
+        self.ns = []
+
+        self.inv_ns = []
+        self.mx = []
+        self.a = []
+        self.ptr = []
+        self.add_ns(self.dns_name, self.ip)
+
+    def add_ns(self, name, ip, iprange=""):
+        if ["",name] not in self.ns:
+            self.ns.append(["", name])
+            self.inv_ns.append(
+                [iprange, name if name.endswith(".") else name + "." + self.origin]
+            )
+        self.add_a(name, ip)
+
+    def add_mx(self, name, ip, priority):
+        for mx in self.mx:
+            if mx[1]==name:
+                mx[0]=priority
+                return
+        self.mx.append([priority, name])
+        self.add_a(name, ip)
+
+    def add_a(self, name, ip):
+        for i,a in enumerate(self.a):
+            if a[0]==name:
+                self.a[i][1]=ip
+                self.ptr[i][0]=".".join(ip.split(".")[-self.octets :][::-1])
+                return
+            elif a[1] ==ip:
+                self.a[i][0]=name
+                self.ptr[i][1]=name if name.endswith(".") else name + "." + self.origin
+                return
+                
+        self.a.append([name, ip])
+        self.ptr.append(
+            [
+                ".".join(ip.split(".")[-self.octets :][::-1]),
+                name if name.endswith(".") else name + "." + self.origin,
+            ]
+        )
+
+    def generate_db_file(self):
+        dir_content = f"$ORIGIN {self.origin}\n"
+        dir_content += f"$TTL {self.ttl}\n"
+        dir_content += "\n"
+        dir_content += f"@\tIN\tSOA\t{self.dns_name} {self.admin} (\n"
+        dir_content += "\t\t\t2018111201\n"  # serial
+        dir_content += "\t\t\t86400\n"  # refresh
+        dir_content += "\t\t\t7200\n"  # retry
+        dir_content += "\t\t\t2419200\n"  # expire
+        dir_content += "\t\t\t3600)\n"  # neg. TTL
+        for row in self.ns:
+            cell0 = row[0] if row[0] != "" else "\t"
+            dir_content += f"{cell0}\tNS\t{row[1]}\n"
+        for row in self.mx:
+            dir_content += f"\t\tMX\t{row[0]} {row[1]}\n"
+        for row in self.a:
+            cell0 = row[0] if row[0] != "" else "\t"
+            dir_content += f"{cell0}\t\tA\t{row[1]}\n"
+        inv_content = f"$ORIGIN {self.inv_origin}\n"
+        inv_content += f"$TTL {self.ttl}\n"
+        inv_content += "\n"
+        inv_content += f"@\tIN\tSOA\t{self.inv_name} {self.inv_admin} (\n"
+        inv_content += "\t\t\t2018111201\n"  # serial
+        inv_content += "\t\t\t86400\n"  # refresh
+        inv_content += "\t\t\t7200\n"  # retry
+        inv_content += "\t\t\t2419200\n"  # expire
+        inv_content += "\t\t\t3600)\n"  # neg. TTL
+        for row in self.inv_ns:
+            cell0 = row[0] if row[0] != "" else "\t"
+            inv_content += f"{cell0}\tNS\t{row[1]}\n"
+        for row in self.ptr:
+            inv_content += f"{row[0]}\t\tPTR\t{row[1]}\n"
+        return dir_content, inv_content
+
+    def print(self):
+        dir_content, inv_content = self.generate_db_file()
+        Console().print("Direct", style="bold cyan")
+        Console().print(dir_content)
+        Console().print("Inverse", style="bold magenta")
+        Console().print(inv_content)
 
 
-def generate_zone(origin, ip, mask, file, invfile, dns_ip=None, slave_ip=None):
+def generate_zone(
+    origin: str,
+    ip: str,
+    mask: int,
+    file: str,
+    invfile: str,
+    dns_ip: str = None,
+    slave_ips: List[str] = None,
+) -> tuple[str, None] | tuple[str, List[str]]:
 
     invorigin = ".".join(ip.split(".")[: mask // 8][::-1]) + ".in-addr.arpa"
     # TTL=IntPrompt.ask("TTL:" default=64000)
@@ -24,8 +124,8 @@ def generate_zone(origin, ip, mask, file, invfile, dns_ip=None, slave_ip=None):
         + "    type master;\n"
         + f'    file "{file}";\n'
         + (
-            ("    allow-transfer { " + f"{slave_ip};" + " };\n")
-            if slave_ip != "" and slave_ip is not None
+            ("    allow-transfer { " + ";".join(slave_ips) + ";" + " };\n")
+            if slave_ips is not None and slave_ips != ""
             else ""
         )
         + "};\n\n"
@@ -34,36 +134,40 @@ def generate_zone(origin, ip, mask, file, invfile, dns_ip=None, slave_ip=None):
         + "    type master;\n"
         + f'    file "{invfile}";\n'
         + (
-            ("    allow-transfer { " + f"{slave_ip};" + " };\n")
-            if slave_ip != "" and slave_ip is not None
+            ("    allow-transfer { " + ";".join(slave_ips) + ";" + " };\n")
+            if slave_ips is not None and slave_ips != ""
             else ""
         )
         + "};\n"
     )
-    if slave_ip == None:
-        return name_conf_loca_master
-    name_conf_loca_slave = (
-        f'zone "{origin}"'
-        + " {\n"
-        + "    type master;\n"
-        + f'    file "{file}";\n'
-        + "    masters { "
-        + f"{dns_ip};"
-        + " };\n"
-        + "    masterfile-format text;\n"
-        + "};\n\n"
-        + f'zone "{invorigin}"'
-        + " {\n"
-        + "    type master;\n"
-        + f'    file "{invfile}";\n'
-        + "    masters { "
-        + f"{dns_ip};"
-        + " };\n"
-        + "    masterfile-format text;\n"
-        + "};\n"
-    )
+    if slave_ips is None or len(slave_ips) == 0:
+        return name_conf_loca_master, None
 
-    return name_conf_loca_master, name_conf_loca_slave
+    name_conf_loca_slaves = [
+        (
+            f'zone "{origin}"'
+            + " {\n"
+            + "    type slave;\n"
+            + f'    file "{file}";\n'
+            + "    masters { "
+            + f"{dns_ip};"
+            + " };\n"
+            + "    masterfile-format text;\n"
+            + "};\n\n"
+            + f'zone "{invorigin}"'
+            + " {\n"
+            + "    type slave;\n"
+            + f'    file "{invfile}";\n'
+            + "    masters { "
+            + f"{dns_ip};"
+            + " };\n"
+            + "    masterfile-format text;\n"
+            + "};\n"
+        )
+        for ip in slave_ips
+    ]
+
+    return name_conf_loca_master, name_conf_loca_slaves
 
 
 def run():
@@ -77,12 +181,86 @@ def run():
     dns_ip = Prompt.ask("enter the master dns ip:")
     dns_name = Prompt.ask("enter the master dns name:")
     slave_ip = Prompt.ask("enter the slave dns ip:")
-    if slave_ip == "":
+    slave_name = ""
+    if slave_ip != "":
         slave_name = Prompt.ask("Enter the name of the slave:")
     dbfile = Prompt.ask("File for the direct RR:", default=origin.slit(".")[0] + ".db")
     invdbfile = Prompt.ask(
         "File for the inverse RR:", default=origin.slit(".")[0] + "_inv.db"
     )
+    masterconf, slaveconf = generate_zone(
+        origin, ip, mask, dbfile, invdbfile, dns_ip, slave_ip
+    )
+    console = Console()
+    console.print(masterconf)
+    console.print(slaveconf)
 
     if not (origin.endswith(".")):
         origin = origin + "."
+
+
+def read_toml(file: str):
+    console = Console()
+    with open(file, "rb") as f:
+        data = tomllib.load(f)
+    console.print(data)
+    origin = [key for key in data.keys() if "RR" != key][0]
+    conf = data[origin]
+    rr_data = data["RR"]
+
+    master_conf, slave_confs = generate_zone(
+        origin,
+        conf["iprange"],
+        conf["mask"],
+        conf["dirfile"],
+        conf["invfile"],
+        conf["ip_master"],
+        conf["ip_slaves"],
+    )
+    console.print(conf["master"], style="bold cyan")
+    console.print(master_conf)
+    for slave, slave_conf in zip(conf["slaves"], slave_confs):
+        console.print(slave, style="bold magenta")
+        console.print(slave_conf)
+    if not (origin.endswith(".")):
+        origin = origin + "."
+    invorigin = (
+        ".".join(conf["iprange"].split(".")[: conf["mask"] // 8][::-1])
+        + ".in-addr.arpa."
+    )
+
+    rrtable = RRTable(
+        origin,
+        conf["ttl"],
+        conf["master_hostname"],
+        conf["admin"],
+        invorigin,
+        conf["ip_master"],
+    )
+    # rrtable.print()
+    # console.print(rr_data)
+    for rr in rr_data:
+        # console.print(rr)
+        match rr["type"]:
+            case "NS":
+                rrtable.add_ns(
+                    rr["name"], rr["ip"], rr["iprange"] if "iprange" in rr else ""
+                )
+                continue
+            case "MX":
+                rrtable.add_mx(
+                    rr["name"], rr["ip"], rr["priority"]
+                )
+                continue
+            case "A":
+                rrtable.add_a(
+                    rr["name"], rr["ip"]
+                )
+                continue
+    rrtable.print()
+    return master_conf,slave_confs, rrtable
+            
+                
+
+if __name__=="__main__":
+    read_toml("dns.toml")
--- tests/test_dns.py
+++ tests/test_dns.py
@@ -1,12 +1,22 @@
+from pytest import fixture
 from rich.console import Console
 from rich.columns import Columns
 from rich.panel import Panel
 
-from dns import *
+from dns import RRTable, generate_zone, read_toml
 
 
-def test_genrate_zone_master():
-    console = Console()
+@fixture
+def rr():
+    return RRTable("lab.api.", 64000, "ns", "admin", "1.0.10.in-addr.arpa.", "10.0.1.2")
+
+
+@fixture
+def console():
+    return Console(width=160)
+
+
+def test_genrate_zone_master(console):
     expected = """zone "lab.api" {
     type master;
     file "lab.db";
@@ -17,7 +27,7 @@ zone "1.0.10.in-addr.arpa" {
     file "lab_inv.db";
 };
 """
-    result = generate_zone("lab.api", "10.0.1.0", 24, "lab.db", "lab_inv.db")
+    result, _ = generate_zone("lab.api", "10.0.1.0", 24, "lab.db", "lab_inv.db")
     columns = Columns()
     columns.add_renderable(Panel(expected))
     columns.add_renderable(Panel(result))
@@ -39,32 +49,253 @@ zone "1.0.10.in-addr.arpa" {
     allow-transfer { 10.0.1.3; };
 };
 """
-    expected_slave = """zone "lab.api" {
-    type master;
+    expected_slaves = [
+        """zone "lab.api" {
+    type slave;
     file "lab.db";
     masters { 10.0.1.2; };
     masterfile-format text;
 };
 
 zone "1.0.10.in-addr.arpa" {
-    type master;
+    type slave;
     file "lab_inv.db";
     masters { 10.0.1.2; };
     masterfile-format text;
 };
 """
-    result_master, result_slave = generate_zone(
-        "lab.api", "10.0.1.0", 24, "lab.db", "lab_inv.db", "10.0.1.2", "10.0.1.3"
+    ]
+    result_master, result_slaves = generate_zone(
+        "lab.api", "10.0.1.0", 24, "lab.db", "lab_inv.db", "10.0.1.2", ["10.0.1.3"]
     )
+
     columns = Columns()
     subcolumns_master = Columns()
     subcolumns_master.add_renderable(Panel(expected_master, title="Expected"))
     subcolumns_master.add_renderable(Panel(result_master, title="Result"))
     columns.add_renderable(Panel(subcolumns_master, title="Master"))
     subcolumns_slave = Columns()
-    subcolumns_slave.add_renderable(Panel(expected_slave, title="Expected"))
-    subcolumns_slave.add_renderable(Panel(result_slave, title="Result"))
+    subcolumns_slave.add_renderable(Panel(expected_slaves[0], title="Expected"))
+    subcolumns_slave.add_renderable(Panel(result_slaves[0], title="Result"))
     columns.add_renderable(Panel(subcolumns_slave, title="Slave"))
     console.print(columns)
     assert result_master == expected_master
-    assert result_slave == expected_slave
+    assert result_slaves[0] == expected_slaves[0]
+
+
+def test_rr_simple_dir(rr, console):
+    expected = """$ORIGIN lab.api.
+$TTL 64000
+
+@	IN	SOA\tns admin (
+			2018111201
+			86400
+			7200
+			2419200
+			3600)
+		NS	ns
+ns		A	10.0.1.2
+"""
+    result, _ = rr.generate_db_file()
+
+    columns = Columns(width=80)
+    columns.add_renderable(Panel(str(expected)))
+    columns.add_renderable(Panel(str(result)))
+    console.print(columns)
+    assert result == expected
+
+
+def test_rr_simple_invr(rr, console):
+    expected = """$ORIGIN 1.0.10.in-addr.arpa.
+$TTL 64000
+
+@	IN	SOA\tns.lab.api. admin.lab.api. (
+			2018111201
+			86400
+			7200
+			2419200
+			3600)
+		NS	ns.lab.api.
+2		PTR	ns.lab.api.
+"""
+    _, result = rr.generate_db_file()
+
+    columns = Columns(width=40)
+    columns.add_renderable(Panel(str(expected)))
+    columns.add_renderable(Panel(str(result)))
+    console.print(columns)
+    assert result == expected
+
+
+def test_rr_mx_dir(rr, console):
+    expected = """$ORIGIN lab.api.
+$TTL 64000
+
+@	IN	SOA\tns admin (
+			2018111201
+			86400
+			7200
+			2419200
+			3600)
+		NS	ns
+		MX	10 mail
+ns		A	10.0.1.2
+mail		A	10.0.1.4
+"""
+    rr.add_mx("mail", "10.0.1.4", 10)
+    result, _ = rr.generate_db_file()
+
+    columns = Columns()
+    columns.add_renderable(Panel(str(expected), width=70))
+    columns.add_renderable(Panel(str(result), width=70))
+    console.print(columns)
+    assert result == expected
+
+
+def test_rr_mx_invr(rr, console):
+    expected = """$ORIGIN 1.0.10.in-addr.arpa.
+$TTL 64000
+
+@	IN	SOA\tns.lab.api. admin.lab.api. (
+			2018111201
+			86400
+			7200
+			2419200
+			3600)
+		NS	ns.lab.api.
+2		PTR	ns.lab.api.
+4		PTR	mail.lab.api.
+"""
+    rr.add_mx("mail", "10.0.1.4", 10)
+    _, result = rr.generate_db_file()
+
+    columns = Columns(width=40)
+    columns.add_renderable(Panel(str(expected)))
+    columns.add_renderable(Panel(str(result)))
+    console.print(columns)
+    assert result == expected
+
+
+def test_rr_ns_dir(rr, console):
+    expected = """$ORIGIN lab.api.
+$TTL 64000
+
+@	IN	SOA\tns admin (
+			2018111201
+			86400
+			7200
+			2419200
+			3600)
+		NS	ns
+		NS	ns2
+ns		A	10.0.1.2
+ns2		A	10.0.1.3
+"""
+    rr.add_ns("ns2", "10.0.1.3")
+    result, _ = rr.generate_db_file()
+
+    columns = Columns()
+    columns.add_renderable(Panel(str(expected), width=70))
+    columns.add_renderable(Panel(str(result), width=70))
+    console.print(columns)
+    assert result == expected
+
+
+def test_rr_ns_invr(rr, console):
+    expected = """$ORIGIN 1.0.10.in-addr.arpa.
+$TTL 64000
+
+@	IN	SOA\tns.lab.api. admin.lab.api. (
+			2018111201
+			86400
+			7200
+			2419200
+			3600)
+		NS	ns.lab.api.
+		NS	ns2.lab.api.
+2		PTR	ns.lab.api.
+3		PTR	ns2.lab.api.
+"""
+    rr.add_ns("ns2", "10.0.1.3")
+    _, result = rr.generate_db_file()
+
+    columns = Columns(width=40)
+    columns.add_renderable(Panel(str(expected)))
+    columns.add_renderable(Panel(str(result)))
+    console.print(columns)
+    assert result == expected
+
+
+def test_rr_a_dir(rr, console):
+    expected = """$ORIGIN lab.api.
+$TTL 64000
+
+@	IN	SOA\tns admin (
+			2018111201
+			86400
+			7200
+			2419200
+			3600)
+		NS	ns
+ns		A	10.0.1.2
+www		A	10.0.1.5
+"""
+    rr.add_a("www", "10.0.1.5")
+    result, _ = rr.generate_db_file()
+
+    columns = Columns()
+    columns.add_renderable(Panel(str(expected), width=70))
+    columns.add_renderable(Panel(str(result), width=70))
+    console.print(columns)
+    assert result == expected
+
+
+def test_rr_a_invr(rr, console):
+    expected = """$ORIGIN 1.0.10.in-addr.arpa.
+$TTL 64000
+
+@	IN	SOA\tns.lab.api. admin.lab.api. (
+			2018111201
+			86400
+			7200
+			2419200
+			3600)
+		NS	ns.lab.api.
+2		PTR	ns.lab.api.
+5		PTR	www.lab.api.
+"""
+    rr.add_a("www", "10.0.1.5")
+    _, result = rr.generate_db_file()
+
+    columns = Columns(width=40)
+    columns.add_renderable(Panel(str(expected)))
+    columns.add_renderable(Panel(str(result)))
+    console.print(columns)
+    assert result == expected
+
+
+def test_read_toml_dir(console):
+    expected = """$ORIGIN lab.api.
+$TTL 64000
+@	IN	SOA ns admin (
+			2018111201
+			86400
+			7200
+			2419200
+			3600)
+		NS	ns
+		NS	ns2
+		MX	10 mail
+ns		A	10.0.1.2
+ns2		A	10.0.1.3
+mail	A	10.0.1.4
+www		A	10.0.1.5
+usr1	A	10.0.1.66
+usr2	A	10.0.1.98"""
+    _, _, rrtable = read_toml("tests/dns.toml")
+    result, _ = rrtable.generate_db_file()
+    columns = Columns()
+    columns.add_renderable(Panel(str(expected), width=70))
+    columns.add_renderable(Panel(str(result), width=70))
+    console.print(columns)
+    assert result == expected
